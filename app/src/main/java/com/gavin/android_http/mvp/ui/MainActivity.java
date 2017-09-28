34cef5f63b131d5ea4ccc6cbf67a63506c5cb118
GeekGhc <ghcgavin@sina.com>
2017-09-28 21:56:09 +0800

    add utils

--- app/src/main/java/com/gavin/android_http/mvp/ui/MainActivity.java
+++ app/src/main/java/com/gavin/android_http/mvp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.gavin.android_http;
+package com.gavin.android_http.mvp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.gavin.android_http.R;
 
 public class MainActivity extends AppCompatActivity {
 
